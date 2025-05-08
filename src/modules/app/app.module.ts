9def0a593b0cf64d23e42cd5327bb7e8d53f9643
Saeed Norouzi <saeednorouzi98@gmail.com>
2025-05-08 17:17:10 +0330

    feat(database): Integrate Mongoose for MongoDB connection
    Description:
    This commit integrates Mongoose to establish a connection with a MongoDB database.
    
    Logs:
    - Installs `@nestjs/mongoose` and `mongoose` dependencies.
    - Registers the Mongoose module with the NestJS application, configuring it to connect to a local MongoDB instance at `mongodb://localhost:27017/nestjs-graphql`.
    - Updates the `package.json` to include the new dependencies and update the format script.
    - Adds `@types/mongoose` as a dev dependency.

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [],
+	imports: [
+		/** Register MongoDB connection */
+		MongooseModule.forRoot('mongodb://localhost:27017/nestjs-graphql'),
+	],
 })
 export class AppModule {}
